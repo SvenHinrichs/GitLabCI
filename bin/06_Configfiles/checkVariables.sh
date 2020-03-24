638cb7e94c9038fd1d4f53816ae1305c6f3e6e9b
sven.hinrichs <sven.hinrichs@rwth-aachen.de>
2020-03-24 11:10:43 +0100

    Check Setting

---
+++ bin/06_Configfiles/checkVariables.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env 
+
+set -e
+
+if [ -z "$GL_TOKEN" ]; then
+  echo "GL_TOKEN not set"
+  echo "Please set the GitLab Private Token as GL_TOKEN"
+  exit 1
+fi
+
+set -e
+
+if [ -z "$TARGET_BRANCH " ]; then
+  echo "TARGET_BRANCH  not set"
+  echo "Please set your current branch in .gitlab-ci.yml "
+  exit 1
+fi
+
+echo "All required Variables are set. CI-Tests can run. "
