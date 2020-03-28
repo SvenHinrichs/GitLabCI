1f18a7b74d620a58b823896cb44ce20227de0f18
sven.hinrichs <sven.hinrichs@rwth-aachen.de>
2020-03-28 15:51:56 +0100

    Pull Request in Github

---
+++ bin/06_Configfiles/autoPullRequest.sh
@@ -0,0 +1,10 @@
+
+curl --location --request POST 'https://api.github.com/repos/SvenHinrichs/GitLabCI/pulls' \
+--header 'Authorization: $GITHUB_API_TOKEN' \
+--header 'Content-Type: application/json' \
+--data-raw '{
+    "title": "HTML Correction",
+    "body": "Please pull these awesome changes in!",
+    "head": "SvenHinrichs:${Newbranch}",
+    "base": "${TARGET_BRANCH}"
+}'
