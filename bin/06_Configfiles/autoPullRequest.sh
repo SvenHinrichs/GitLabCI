06b02ffedeac5a45d5f2728e524abd6efc99f874
sven.hinrichs <sven.hinrichs@rwth-aachen.de>
2020-03-29 13:15:31 +0200

    Add masked GL Token

--- bin/06_Configfiles/autoPullRequest.sh
+++ bin/06_Configfiles/autoPullRequest.sh
@@ -5,15 +5,15 @@ set -e
 echo "Create a Pull request ${Newbranch} to ${TARGET_BRANCH}"
 echo $GITHUB_API_TOKEN
 
-curl --location --request POST 'https://api.github.com/repos/SvenHinrichs/GitLabCI/pulls' \
---header 'Authorization:Bearer ${GITHUB_API_TOKEN}' \
---header 'Content-Type: application/json' \
---data '{
-    "title": "HTML Correction",
-    "body": "Correct the HTML Code",
-    "head": "SvenHinrichs:Correct_HTML_master",
-    "base": "master"
-}'
+curl -X POST "https://api.github.com/repos/SvenHinrichs/GitLabCI/pulls" \
+	--header "Authorization:Bearer ${GITHUB_API_TOKEN}" \
+	--header "Content-Type: application/json" \
+	--data "{
+		\"title\": \"HTML Correction\",
+		\"body\": \"Correct the HTML Code\",
+		\"head\": \"SvenHinrichs:${Newbranch}\",
+		\"base\": \"${TARGET_BRANCH}\"
+}"
 
 echo "Create pull request. Merge ${Newbranch} into ${TARGET_BRANCH}."
 
