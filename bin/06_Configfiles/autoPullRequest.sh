9acf143807b928cc626bfee8909fc37f5d181e6a
sven.hinrichs <sven.hinrichs@rwth-aachen.de>
2020-03-29 12:24:49 +0200

    Add API-GITHUB-TOKEN

--- bin/06_Configfiles/autoPullRequest.sh
+++ bin/06_Configfiles/autoPullRequest.sh
@@ -3,13 +3,14 @@ set -e
 
 
 echo "Create a Pull Request ${Newbranch} to ${TARGET_BRANCH}"
+echo $GITHUB_API_TOKEN
 
 curl --location --request POST 'https://api.github.com/repos/SvenHinrichs/GitLabCI/pulls' \
 --header 'Authorization: Bearer ${GITHUB_API_TOKEN}' \
 --header 'Content-Type: application/json' \
 --data-raw '{
     "title": "HTML Correction",
-    "body": "Please pull these awesome changes in!",
+    "body": "Correct the HTML Code",
     "head": "SvenHinrichs:${Newbranch}",
     "base": "${TARGET_BRANCH}"
 }'
