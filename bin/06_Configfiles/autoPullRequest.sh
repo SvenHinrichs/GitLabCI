76932c07958e5a63f27ef434ef40b819a0eb6d4a
sven.hinrichs <sven.hinrichs@eonerc.rwth-aachen.de>
2020-04-08 19:28:24 +0200

    Update autopullRequest.sh

--- bin/06_Configfiles/autoPullRequest.sh
+++ bin/06_Configfiles/autoPullRequest.sh
@@ -10,16 +10,18 @@ set -e
 
 
 echo "Create a Pull request ${Newbranch} to ${TARGET_BRANCH}"
+echo "${Github_Repository}"
+echo "${GitHub_Username}:${Newbranch}"
 
 curl -X POST "https://api.github.com/repos/${Github_Repository}/pulls" \
 	--header "Authorization:Bearer ${GITHUB_API_TOKEN}" \
 	--header "Content-Type: application/json" \
 	--data "{
-		\"title\": \"Correct HTML $[TARGET_BRANCH}\",
+		\"title\": \"Correct HTML ${TARGET_BRANCH}\",
 		\"body\": \"Merge the Corrected HTML Code\",
 		\"head\": \"${GitHub_Username}:${Newbranch}\",
 		\"base\": \"${TARGET_BRANCH}\"
 }"
 
-echo "Create pull request. Merge ${Newbranch} into ${TARGET_BRANCH}."
+echo "Create pull request. Merge ${Newbranch} into ${TARGET_BRANCH}. "
 exit 0 
