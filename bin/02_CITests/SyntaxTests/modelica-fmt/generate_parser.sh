ec09aa771d4bbbac54558842d65985f0d64167ac
sven.hinrichs <sven.hinrichs@rwth-aachen.de>
2020-09-10 13:47:17 +0200

    Add regression Skript

--- bin/02_CITests/SyntaxTests/modelica-fmt/generate_parser.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker build -t antlr4:latest -f thirdparty/Dockerfile-Antlr .
-
-docker run -v "$(pwd)/thirdparty":/var/antlrResult \
-        antlr4:latest \
-        -Dlanguage=Go -o /var/antlrResult/parser /var/antlrResult/Modelica.g4
