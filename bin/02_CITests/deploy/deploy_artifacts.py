d4453054c5ce2fbc605326c6d8dc38bfe7af3b38
SvenHinrichs <sven.hinrichs@eonerc.rwth-aachen.de>
2021-11-10 13:58:37 +0100

    ci_update_ref

--- bin/02_CITests/deploy/deploy_artifacts.py
+++ bin/02_CITests/deploy/deploy_artifacts.py
@@ -3,7 +3,7 @@ import codecs
 import sys
 import shutil
 import argparse
-
+import glob
 
 class Deploy_Artifacts(object):
 
@@ -16,8 +16,9 @@ class Deploy_Artifacts(object):
         self.CEND = '\033[0m'
 
         sys.path.append('bin/02_CITests')
-        from _config import new_ref_file
+        from _config import new_ref_file, update_ref_file
         self.new_ref_file = new_ref_file
+        self.update_ref_file = update_ref_file
 
     def _get_changed_ref(self):  # list all changed reference results to a list
         changed_file = codecs.open(self.new_ref_file, "r", encoding='utf8')
@@ -34,11 +35,28 @@ class Deploy_Artifacts(object):
         changed_file.close()
         return changed_ref
 
+    def _get_update_ref(self):
+        updated_file = codecs.open(self.update_ref_file, "r", encoding='utf8')
+        lines = updated_file.readlines()
+        updated_ref = []
+        for line in lines:
+            if line.find("txt") > -1:
+                line = line.strip()
+                updated_ref.append(f'{self.library}{os.sep}Resources{os.sep}ReferenceResults{os.sep}Dymola{os.sep}{line.strip()}')
+                continue
+            else:
+                continue
+        updated_file.close()
+        return updated_ref
+
     def copy_txt(self, changed_ref):  # Copy reference results from AixLib\Resources\ReferenceResults\Dymola\* to Referencefiles\\*
         if os.path.exists(self.folder) is False:
             os.mkdir(self.folder)
+        else:
+            files = os.listdir(self.folder)
+            for file in files:
+                os.remove(f'{self.folder}{os.sep}{file}')
         for ref in changed_ref:
-            print(ref)
             destination = self.folder + os.sep + ref[ref.rfind(os.sep):]
             try:
                 shutil.copy(ref, destination)
@@ -46,19 +64,26 @@ class Deploy_Artifacts(object):
             except FileNotFoundError:
                 print(f'{self.CRED}Cannot find folder:{self.CEND} {destination}')
 
-
-
 if __name__ == '__main__':
-    #  python bin/02_CITests/deploy/deploy_artifacts.py --library AixLib --ref
     parser = argparse.ArgumentParser(description='deploy artifacts')
     unit_test_group = parser.add_argument_group("arguments to run deploy artifacts")
     unit_test_group.add_argument("-L", "--library", default="AixLib", help="Library to test")
     unit_test_group.add_argument("--ref", help='Deploy new reference files', action="store_true")
+    unit_test_group.add_argument("--new-ref",
+                                 help="Plot new models with new created reference files",
+                                 action="store_true")
+    unit_test_group.add_argument("--updated-ref",
+                                 help="Plot new models with new created reference files",
+                                 action="store_true")
     args = parser.parse_args()
-
     from deploy_artifacts import Deploy_Artifacts
 
     if args.ref is True:
         ref_artifact = Deploy_Artifacts(library=args.library)
-        changed_ref = ref_artifact._get_changed_ref()
-        ref_artifact.copy_txt(changed_ref)
+        if args.new_ref is True:  # python bin/02_CITests/deploy/deploy_artifacts.py --library AixLib --ref --new-ref
+            changed_ref = ref_artifact._get_changed_ref()
+            ref_artifact.copy_txt(changed_ref)
+        if args.updated_ref is True:  # python bin/02_CITests/deploy/deploy_artifacts.py --library AixLib --ref --updated-ref
+            updated_ref = ref_artifact._get_update_ref()
+            ref_artifact.copy_txt(updated_ref)
+
