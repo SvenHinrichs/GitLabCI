6a498f0571ebd4d7af2102b121d65b38c9ca5e1b
SvenHinrichs <sven.hinrichs@eonerc.rwth-aachen.de>
2021-11-16 12:27:32 +0100

    ci_regression_test

--- bin/02_CITests/_config.py
+++ bin/02_CITests/_config.py
@@ -44,7 +44,7 @@ setting_file = f'bin{os.sep}09_Setting{os.sep}CI_setting.txt'
 # image_name
 image_name = 'registry.git.rwth-aachen.de/ebc/ebc_intern/dymola-docker:miniconda-latest'
 project_name = 'SvenHinrichs/GitLabCI'
-variable_main_list = ['Praefix_Branch: "Correct_HTML_"', 'TARGET_BRANCH: $CI_COMMIT_REF_NAME', 'Newbranch: ${Praefix_Branch}${CI_COMMIT_REF_NAME}', 'Github_Repository: SvenHinrichs/GitLabCI', 'GITLAB_Page: "https://svenhinrichs.pages.rwth-aachen.de/GitLabCI"']
+variable_main_list = ['Github_Repository: SvenHinrichs/GitLabCI', 'GITLAB_Page: "https://svenhinrichs.pages.rwth-aachen.de/GitLabCI"']
 stage_list = ["Ref_Check", "build", "HTML_Check", "IBPSA_Merge", "create_html_whitelist", "Update_WhiteList", "Release", "StyleCheck", "check", "openMR", "post", "create_whitelist", "simulate", "RegressionTest", "Update_Ref", "plot_ref", "prepare", "deploy"]
 # Pull Request Comment
 #self.post_comment_message = f'Error in regression test.\\n Compare the results on the following page {self.page_url}'
